37db1d687f980169c2835b0e6d636f187bfb9555
micaela <micaela.pineda@selu.edu>
2021-10-26 20:11:04 +0000

    added a script to order the data by date and indiviadual ID, then filtered out one specific bat

---
+++ R/Filtered_bat.R
@@ -0,0 +1,8 @@
+noctules_2 <- read.csv("C:\\Users\\12093\\Documents\\Bat data\\Nyctalus.csv")
+noctules2<- noctules_2 %>% 
+  rename(ID = individual.local.identifier)
+data_ordered <- noctules2[with(noctules2, order(ID, timestamp)),]
+data_ordered
+ffnt <- data_ordered %>%
+  filter(ID == 5493)
+ffnt
